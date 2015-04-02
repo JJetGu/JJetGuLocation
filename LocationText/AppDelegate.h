81ae43816813a6da14a67b28e501a0f6067aa173
JJetGu <mac@20141111-Mac.local>
2015-04-02 15:50:35 +0300

    Initial Commit

---
+++ LocationText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationText
+//
+//  Created by JJetGu on 15-4-2.
+//  Copyright (c) 2015年 HM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationText/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LocationText
+//
+//  Created by JJetGu on 15-4-2.
+//  Copyright (c) 2015年 HM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
